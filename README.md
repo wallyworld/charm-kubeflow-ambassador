2438796787d67daec0de049830a7e61a49ead3e8
Cory Johns <johnsca@gmail.com>
2018-07-09 12:40:52 -0400

    Initial skeleton for kubeflow-amabassador charm

---
+++ README.md
@@ -0,0 +1,5 @@
+Overview
+========
+
+This charm deploys the Ambassador API Gateway configured for use with
+Kubeflow to Kubernetes models in Juju.
---
+++ reactive/kubeflow_ambassador.py
@@ -0,0 +1,47 @@
+import yaml
+
+from charms.reactive import set_flag
+from charms.reactive import when_not
+
+from charms import layer
+from charms.layer.basic import pod_spec_set
+
+
+@when_not('charm.kubeflow-ambassador.started')
+def start_charm():
+    layer.status.maintenance('configuring ambassador container')
+
+    pod_spec_set(yaml.dump({
+        'containers': [
+            {
+                'name': 'ambassador',
+                'image': 'quay.io/datawire/ambassador:0.30.1',
+                'command': [],
+                'ports': [
+                    {
+                        'name': 'ambassador',
+                        'containerPort': 8080,
+                    },
+                ],
+                'livenessProbe': {
+                    'httpGet': {
+                        'path': '/ambassador/v0/check_alive',
+                        'port': 8877,
+                    },
+                    'initialDelaySeconds': 30,
+                    'periodSeconds': 30,
+                },
+                'readinessProbe': {
+                    'httpGet': {
+                      'path': '/ambassador/v0/check_ready',
+                      'port': 8877,
+                    },
+                    'initialDelaySeconds': 30,
+                    'periodSeconds': 30,
+                },
+            },
+        ],
+    }))
+
+    layer.status.active('ready')
+    set_flag('charm.kubeflow-ambassador.started')
