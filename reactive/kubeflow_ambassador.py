44445a4c3f83b0e9a2b01c850e6f8dfdddeac7af
Cory Johns <johnsca@gmail.com>
2018-07-25 17:20:33 -0400

    Use resource for image
    Also include missed env config var

--- reactive/kubeflow_ambassador.py
+++ reactive/kubeflow_ambassador.py
@@ -1,21 +1,32 @@
-import yaml
+import os
 
 from charms.reactive import set_flag
-from charms.reactive import when_not
+from charms.reactive import when, when_not
 
 from charms import layer
-from charms.layer.basic import pod_spec_set
 
 
+@when_not('layer.docker-resource.ambassador-image.fetched')
+def fetch_image():
+    layer.docker_resource.fetch('ambassador-image')
+
+
+@when('layer.docker-resource.ambassador-image.fetched')
 @when_not('charm.kubeflow-ambassador.started')
 def start_charm():
     layer.status.maintenance('configuring container')
 
-    pod_spec_set(yaml.dump({
+    image_info = layer.docker_resource.get_info('ambassador-image')
+
+    layer.caas_base.pod_spec_set({
         'containers': [
             {
                 'name': 'ambassador',
-                'image': 'quay.io/datawire/ambassador:0.30.1',
+                'imageDetails': {
+                    'imagePath': image_info.registry_path,
+                    'username': image_info.username,
+                    'password': image_info.password,
+                },
                 'command': [],
                 'ports': [
                     {
@@ -23,6 +34,10 @@ def start_charm():
                         'containerPort': 8080,
                     },
                 ],
+                'config': {
+                    'AMBASSADOR_NAMESPACE': os.environ['JUJU_MODEL_NAME'],
+                    'AMBASSADOR_SINGLE_NAMESPACE': 'true',
+                },
                 'livenessProbe': {
                     'httpGet': {
                         'path': '/ambassador/v0/check_alive',
@@ -41,7 +56,7 @@ def start_charm():
                 },
             },
         ],
-    }))
+    })
 
     layer.status.maintenance('creating container')
     set_flag('charm.kubeflow-ambassador.started')
